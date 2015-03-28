0cb7eb8fe75e88c4daebaa4d37ba5f486ac3044b
Tianyu Chen <chentianyu1994@gmail.com>
2015-03-28 11:21:00 -0400

    part(1) Matlab & Simulink

---
+++ Parameters.m
@@ -0,0 +1,9 @@
+function [ plan ] = Parameters( fname )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+    tmp = load(fname);
+    plan.time = tmp(:, 1);
+    plan.p_x = tmp(:, 2);
+    plan.p_y = tmp(:, 3);
+    plan.stance_type = tmp(:, 4);
+end
