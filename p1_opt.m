9e7f6328c0874fa3b804e078b462a3afc156b0e3
summivox <summivox@gmail.com>
2015-04-06 08:44:40 -0400

    wip part 2 (really this is wrong, need complete rewrite)

--- p1_opt.m
+++ p1_opt.m
@@ -8,8 +8,10 @@ ts = (0:dt:t_max).';
 n = length(ts);
 
 % foot position (+ upsampled)
-p_x = [0;plan.p_x];
-p_y = [0;plan.p_y];
+p_x = plan.p_x; p_x = [p_x;p_x(end)];
+p_y = plan.p_y; p_y = [p_y;p_y(end)];
+% p_x = [0;plan.p_x];
+% p_y = [0;plan.p_y];
 t_foot = [0;cumsum(plan.time)];
 p_x_u = zoh(t_foot, p_x, ts);
 p_y_u = zoh(t_foot, p_y, ts);
@@ -23,14 +25,19 @@ function [score, x, y] = f(u)
           + sumsqr(y - p_y_u) + sumsqr(yd) + 30*sumsqr(u_y);
 end
 
-if 1
-    u_opt = fminsearch(@f, zeros(n*2, 1), optimset ...
-        ( 'PlotFcns', @optimplotfval ...
-        ));
+if 0
+    options = optimoptions('fminunc' ...
+        , 'Algorithm', 'quasi-newton' ...
+        , 'PlotFcns', @optimplotfval ...
+        , 'Display', 'iter-detailed' ...
+        );
+    u_opt = fminunc(@f, zeros(n*2, 1), options);
 else
-    u_opt = cmaes(@f, zeros(n*2, 1), 1, cmaes('defaults', struct ...
+    options = cmaes('defaults', struct ...
         ( 'DiagonalOnly', 1 ...
-        )));
+        ));
+    sigma = median(unique(diff(p_x)))/10;
+    u_opt = cmaes(@f, zeros(n*2, 1), sigma, options);
 end
 
 u_x_opt = u_opt(1:n);
