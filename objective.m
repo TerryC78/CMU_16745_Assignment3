2059383d457faa4403f3420501dedd576071cafe
Joe Le Truc <vietexob@gmail.com>
2015-04-06 11:58:52 -0400

    In progress

---
+++ objective.m
@@ -0,0 +1,11 @@
+function [ obj_fun ] = objective( duration )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+% Declare global variables
+global limp_plan;
+
+% Set the duration
+
+end
+
